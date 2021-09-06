02c60142c0f0aef038869a20c82e811339b06327
SunPhobic <69128090+SunPhobic@users.noreply.github.com>
2021-09-06 02:02:16 -0300

    oop simpleEcommerce basic funcionality

--- oopSimpleEcommerce/ooPSimpleEcommerce.js
+++ oopSimpleEcommerce/ooPSimpleEcommerce.js
@@ -7,10 +7,11 @@
         init: function(){
             this.cacheDOM();
             this.bindings();
-            this.populateList(this.stock,this.mainStockList)
+            this.populateList(this.stock,this.stockUl)
         },
 
         cacheDOM: function(){
+            this.body = document.querySelector('body')
             this.header = document.querySelector('header')
             this.title = document.querySelector('#title')
             this.mainStockList = document.querySelector('#mainStockList')
@@ -19,18 +20,72 @@
             this.nameInput = document.querySelector('#nameInput')
             this.priceInput = document.querySelector('#priceInput')
             this.submitButton = document.querySelector('#submitButton')
+            this.cartUl = document.querySelector('#cartUl')
         },
 
         bindings: function(){
-            this.submitButton.addEventListener('click',this.test.bind(this))
+            this.submitButton.addEventListener('click',this.handleSubmit.bind(this))
         },
 
         populateList: function(source,target){
             source.forEach(item=>{
                 const product = document.createElement('li')
                 product.innerHTML = `${item.name}: $${item.price}`
+
+                if(target === this.stockUl){
+                    product.setAttribute('id',item.name)
+                    const addCartButton = document.createElement('button')
+                    addCartButton.innerHTML = 'Add Product'
+                    addCartButton.addEventListener('click',this.addCart.bind(this))
+                    product.appendChild(addCartButton)
+                }
+            
+                if(target === this.cartUl){
+                    product.setAttribute('id',`${item.name}_cart`)
+                    const removeCartButton = document.createElement('button')
+                    removeCartButton.innerHTML = 'Delete Product'
+                    removeCartButton.addEventListener('click',this.removeCart.bind(this))
+                    product.appendChild(removeCartButton)
+                }
+
                 target.appendChild(product)
             })
+        },
+        
+        handleSubmit: function(ev){
+            ev.preventDefault()
+            this.stock.push(
+                {
+                    name:ev.target.parentElement.nameInput.value,
+                    price:parseFloat(ev.target.parentElement.priceInput.value)
+                }
+            )
+            this.stockUl.innerHTML = ''
+            this.populateList(this.stock,this.stockUl)
+        },
+
+        addCart: function(ev){
+            this.stock.forEach(item=>{
+                if(item.name === ev.target.parentElement.id){
+                    this.cart.push(item)
+                }
+            })
+            this.cartUl.innerHTML = ''
+            this.populateList(this.cart,this.cartUl)
+        },
+
+        removeCart: function(ev){
+            const idArr = ev.target.parentElement.id.split('_')
+            const id = idArr[0]
+
+            const findObject = this.cart.find(item=>{
+                return item.name === id
+            })
+            const index = this.cart.indexOf(findObject)
+            this.cart.splice(index,1)
+
+            this.cartUl.innerHTML = ''
+            this.populateList(this.cart,this.cartUl)
         },
 
         test:function(ev){
